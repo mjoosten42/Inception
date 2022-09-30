4f412841056c4435ff175451e396f21d82b1e09a
Maarten Joosten <mjoosten@student.codam.nl>
2022-09-30 15:25:44 +0200

    Use DB_NAME

--- db.php
+++ db.php
@@ -5,12 +5,15 @@
 $hostname = "mariadb";
 $username = $_SERVER["MYSQL_USER"];
 $password = $_SERVER["MYSQL_PASSWORD"];
-$database = "mysql";
+$database = $_SERVER["DB_NAME"];
 
 $conn = mysqli_connect($hostname, $username, $password, $database)
 	or die("Connection failed: " . mysqli_connect_error() . "\n");
 
-$query = mysqli_query($conn, "SELECT user, password, host FROM user")
+mysqli_select_db($conn, $database)
+	or die ("Database selection failed\n");
+
+$query = mysqli_query($conn, "SHOW TABLES;")
 	or die("Query failed: " . mysqli_error($conn) . "\n");
 
 echo "
@@ -25,12 +28,10 @@ echo "
 </style>\n";
 
 echo "<table>";
-echo "<tr><td>"."user"."</td><td>"."password"."</td><td>"."host"."</td></tr>";
+echo "<tr><td>"."Tables in $database"."</td></tr>";
 while ($row = mysqli_fetch_array($query)) {
-	$user = $row['user'];
-	$password = $row['password'];
-	$host = $row['host'];
-	echo "<tr><td>".$user."</td><td>".$password."</td><td>".$host."</td></tr>";
+	$host = $row["Tables_in_$database"];
+	echo "<tr><td>".$host."</td></tr>";
 }
 echo "</table>";
 
--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,22 +1,9 @@
 # SQL files can't read env, so we'll use a heredoc
 
-# This doesn't actually set a root password
-# Disallow remote root login
-# Remove test database
-# Remove anonymous users
-mysql_secure_installation << EOF
-
-	N
-	Y
-	Y
-	Y
-	Y
-EOF
-
 mariadb << EOF
-	SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
-	CREATE DATABASE IF NOT EXISTS wordpress;
-	CREATE OR REPLACE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; 
-	GRANT ALL ON mysql.user TO '$MYSQL_USER'@'%';
+	SET PASSWORD FOR "root"@"localhost" = PASSWORD("$MYSQL_ROOT_PASSWORD");
+	CREATE DATABASE IF NOT EXISTS $DB_NAME;
+	CREATE OR REPLACE USER "$MYSQL_USER"@"%" IDENTIFIED BY "$MYSQL_PASSWORD"; 
+	GRANT ALL ON $DB_NAME.* TO "$MYSQL_USER"@"%";
 	FLUSH PRIVILEGES;
 EOF
